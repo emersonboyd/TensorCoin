6739d5ca9cbe55a5ca932024893fa01f51c5fed8
Connor Arnold <connorarnold@Connors-MacBook-Pro.local>
2017-12-12 16:59:20 -0500

    Beginning to test params

--- bitcoin_rnn.py
+++ bitcoin_rnn.py
@@ -5,11 +5,12 @@ from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import GRU
 from keras.models import load_model
 import pdb
 
-MODEL_NAME = 'bitcoin_model.h5'
-
+MODEL_NAME = 'test.h5'
+PULL_MODEL = '30_days_to_1_day_in_minutes.h5'
 # Get the Bitcoin data from the csv file
 def read_data():
     df = pd.read_csv('./bitcoin_historical-data/coinbaseUSD_1-min_data_2014-12-01_to_2017-10-20.csv.csv')
@@ -21,35 +22,52 @@ def read_data():
 # Get the range of training and testing data e.g.
 # Get the data from 20 days ago to 2 days ago, for every minute
 # segment_data('df,days',20,2,'minute') Can do 'day' 'hour' and 'minute'
-def segment_data(df, rangeUnit, startUnitsBack, endUnitsBack, segmentUnits):
-    if segmentUnits == 'day':
+def segment_data(df, range_unit, start_units_back, end_units_back, segment_units):
+    if segment_units == 'day':
         prices = df.groupby('day')['Weighted_Price'].mean()
-    elif segmentUnits == 'hour':
+    elif segment_units == 'hour':
         prices = df.groupby(['day','hour'])['Weighted_Price'].mean()
-    elif segmentUnits == 'minute':
+    elif segment_units == 'minute':
         prices = df.groupby(['day', 'hour', 'minute'])['Weighted_Price'].mean()
-    if rangeUnit == segmentUnits:
-        startIndex = len(prices) - startUnitsBack
-        endIndex = len(prices) - endUnitsBack
-    if rangeUnit == 'day':
-        if segmentUnits == 'hour':
-            startIndex = len(prices) - 24*startUnitsBack
-            endIndex = len(prices) - 24*endUnitsBack
+    if range_unit == segment_units:
+        start_index = len(prices) - start_units_back
+        end_index = len(prices) - end_units_back
+    if range_unit == 'day':
+        if segment_units == 'hour':
+            start_index = len(prices) - 24 * start_units_back
+            end_index = len(prices) - 24 * end_units_back
         else:
-            startIndex = len(prices) - 24 * 60 * startUnitsBack
-            endIndex = len(prices) - 24 * 60 * endUnitsBack
-    if rangeUnit == 'hour':
-        startIndex = len(prices) - 60 * startUnitsBack
-        endIndex = len(prices) - 60 * endUnitsBack
-    training = prices[startIndex:endIndex]
-    testing =  prices[endIndex:]
+            start_index = len(prices) - 24 * 60 * start_units_back
+            end_index = len(prices) - 24 * 60 * end_units_back
+    if range_unit == 'hour':
+        start_index = len(prices) - 60 * start_units_back
+        end_index = len(prices) - 60 * end_units_back
+    training = prices[start_index:end_index]
+    testing =  prices[end_index:]
     return training, testing
 
 
 def train_data(input, output):
+    #Original inputs
+    # regressor = Sequential()
+    # regressor.add(LSTM(units=4, activation='sigmoid', input_shape=(None, 1)))
+    # regressor.add(Dense(units=1))
+    # regressor.compile(optimizer='adam', loss='mean_squared_error')
+    # regressor.fit(input, output, batch_size=5, epochs=100)
+    # regressor.save(MODEL_NAME)
+    # return regressor
+
+    #Testing Inputs 
     regressor = Sequential()
+    #ADD Layers to the RNN
+    #LSTM: dropout, units,
     regressor.add(LSTM(units=4, activation='sigmoid', input_shape=(None, 1)))
+    #Dense
     regressor.add(Dense(units=1))
+    #Gru
+    #regressor.add(GRU(units=1))
+    #SimpleRNN
+    #Compile RNN, optomizers can be RMSprop
     regressor.compile(optimizer='adam', loss='mean_squared_error')
     regressor.fit(input, output, batch_size=5, epochs=100)
     regressor.save(MODEL_NAME)
@@ -57,6 +75,7 @@ def train_data(input, output):
 
 
 def draw_data(df_test, predicted_price,unit):
+    pdb.set_trace()
     plt.figure(figsize=(15, 15), dpi=80, facecolor='w', edgecolor='k')
     plt.title('BTC Price Prediction', fontsize=40)
     plt.plot(df_test.values, color='red', label='Real BTC Price')
@@ -76,24 +95,29 @@ def draw_data(df_test, predicted_price,unit):
 
 def run():
     df = read_data()
-    segmentUnit = 'minute'
-    df_train, df_test = segment_data(df, 'day',30,1,segmentUnit)
+    create_model = False
+    segment_unit = 'minute'
+    df_train, df_test = segment_data(df, 'minute',100,70,segment_unit)
 
     sc = MinMaxScaler()
 
-    training_set = np.reshape(df_train.values, (len(df_train.values), 1))
-    training_set = sc.fit_transform(training_set)
-    train_input, train_output = training_set[0:len(training_set) - 1], training_set[1:len(training_set)]
-    train_data(np.reshape(train_input, (len(train_input), 1, 1)), train_output)
-
-    regressor = load_model(MODEL_NAME)
+    if create_model:
+        #Reshape the data to create a model
+        training_set = np.reshape(df_train.values, (len(df_train.values), 1))
+        training_set = sc.fit_transform(training_set)
+        train_input, train_output = training_set[0:len(training_set) - 1], training_set[1:len(training_set)]
+        # Create a regressor and save it to MODEL_NAME
+        regressor = train_data(np.reshape(train_input, (len(train_input), 1, 1)), train_output)
+        # load the regressor
+    else:
+        regressor = load_model(PULL_MODEL)
 
     test_input = np.reshape(df_test.values, (len(df_test.values), 1))
     test_input = sc.transform(test_input)
     test_input = np.reshape(test_input, (len(test_input), 1, 1))
     predicted_price = sc.inverse_transform(regressor.predict(test_input))
 
-    draw_data(df_test, predicted_price,segmentUnit)
+    draw_data(df_test, predicted_price,segment_unit)
 
 if __name__ == "__main__":
     run()
